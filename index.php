5c983e8e7c2c3e5e9cdf795f2986f6daa5fdc924
nicole2a <nicoleabgiri@gmail.com>
2024-09-09 10:02:22 +0200

    Home

--- index.php
+++ index.php
@@ -1,25 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
 
-<?php
+  </head>
 
-include "connectie.php";
+  <!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-if(isset($_POST['submit'])){
- $product = $_POST['product'];
- $type = $_POST['type'];
- $fabriek = $_POST['fabriek'];
- $voorraad_product = $_POST['voorraad_product'];
-}
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
 
-$sql = "INSERT INTO 'producten' ('product','type','fabriek','voorraad_product') VALUES ('$Accuboorhamer','$WX382','$Worx','$')"
+    <title>Tools For Ever</title>
+
+    <link href="index.css" rel="stylesheet">
+  </head>
+  <body>
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+<body>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <a class="navbar-brand" href="home.php">tools_for_ever</a>
+      </div>
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="producten.php">producten</a></li>
+        <li><a href="vooraad.php">voorraad</a></li>
+        <li><a href="#"></a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="sign-up.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </nav>
 
 
+    
+</body>
 
-if($result == TRUE) {
-  echo "table created succesfully";
-}
-else{
-  echo "ERROR" . $sql . "<br>". $conn->error;
-}
 
-$conn->close();
-?>
